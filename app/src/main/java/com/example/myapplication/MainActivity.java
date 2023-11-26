1d9e3479bdc04eabc3ac78a70306c06b4484b726
211250016 <211250016@smail.nju.edu.cn>
2023-11-26 22:56:11 +0800

    wheelView

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.myapplication;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,6 +16,11 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
+import com.lukedeighton.wheelview.WheelView;
+import com.lukedeighton.wheelview.adapter.WheelAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +42,33 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-    }
 
+        final WheelView wheelView = (WheelView) findViewById(R.id.wheelView);
+
+        final List<String> entries = new ArrayList<>();
+        for(int i = 0; i < 14; i++) entries.add(String.valueOf(i));
+
+        final List<Drawable> imgList = new ArrayList<>();
+        for(int i = 0; i < 14; i++) imgList.add(getResources().getDrawable(R.mipmap.ic_launcher));
+
+        wheelView.setAdapter(new WheelAdapter() {
+            @Override
+            public Drawable getDrawable(int position) {
+                return imgList.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return imgList.size();
+            }
+        });
+
+        wheelView.setOnWheelItemClickListener(new WheelView.OnWheelItemClickListener() {
+            @Override
+            public void onWheelItemClick(WheelView parent, int position, boolean isSelected) {
+                wheelView.setSelected(position);
+                Log.e("----", "" + position);
+            }
+        });
+    }
 }

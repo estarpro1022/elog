57d7988e04b1d38cab5da312e55d71425ecfffc3
211250016 <211250016@smail.nju.edu.cn>
2023-12-15 17:00:03 +0800

    color change logic

--- app/src/main/java/com/example/myapplication/DiaryActivity.java
+++ app/src/main/java/com/example/myapplication/DiaryActivity.java
@@ -1,7 +1,6 @@
 package com.example.myapplication;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
